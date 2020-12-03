6a39726c0195ddcdb63fb66b3d48bb235996fff9
Denis Sikuler <progwork@yandex.com>
2020-12-03 13:30:39 +0300

    Add types declaration files

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+// Created on the basis of http://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-d-ts.html
+
+export as namespace neva;
+
+export interface BaseEvent {
+    /** The event type. */
+    type: string;
+    [field: string]: unknown;
+}
+
+export interface EventData {
+    /** Value of `params[0]`. */
+    data: any;
+    /** List of additional parameters that are passed besides the event type when the event is emitted. */
+    params: any[];
+    /** The event type. */
+    type: string;
+}
+
+export type EventHandler =
+    | (() => unknown)
+    | ((event: BaseEvent | EventData, ...args: any[]) => unknown);
+
+export interface EventEmitter {
+    hasEventHandler(type?: string, handler?: EventHandler, context?: object): boolean;
+    on(type: string | string[], handler: EventHandler, context?: object): this;
+    off(type?: string, handler?: EventHandler, context?: object): this;
+    emit(type?: string | BaseEvent, ...params: any[]): this;
+}
+
+export function getEmitter<T extends object = object>(target?: T): Omit<T, keyof EventEmitter> & EventEmitter;
+
+export default getEmitter;
